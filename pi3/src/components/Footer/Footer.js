a52285ccc001d7e5a5a01a76f9538eb262ec6c11
TomasLagger1 <tlagger@udesa.edu.ar>
2025-09-01 18:53:40 -0300

    puntos 1 y 2 masomenos hechos pero falto un poco

---
+++ pi3/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+function Footer() {
+  return (
+    <footer>
+        <p>Integrantes: Tomás Lagger, Felix Milia y Fran</p>
+        <p>Copyright © Dashboard 2025</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ pi3/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+function Header() {
+  return (
+    <header>
+        <p>Soy un header</p>
+    </header>
+  )
+}
+
+export default Header
