e3a1fa29564780aa071118873c9aad20dcbf953f
TomasLagger1 <tlagger@udesa.edu.ar>
2025-09-02 16:23:09 -0300

    donervo

--- pi3/src/components/Footer/Footer.js
+++ pi3/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './styles.css'
 function Footer() {
   return (
     <footer>
-        <p>Integrantes: Tomás Lagger, Felix Milia y Fran</p>
+        <p>Integrantes: Tomás Lagger, Felix Milia y Franco Mongiat</p>
         <p>Copyright © Dashboard 2025</p>
     </footer>
   )
--- pi3/src/components/Header/Header.js
+++ pi3/src/components/Header/Header.js
@@ -5,6 +5,10 @@ function Header() {
     <header>
         <img src="/img/logo.png" alt="logo"></img>
         <p>Reels+</p>
+        <nav class="naaaaaavbar">
+             <a href="/">Home</a>
+             <a href="/favoritos">Favoritos</a>
+        </nav>
     </header>
   )
 }
