3ba7697f0dea468b1fcf9026667f2ad2c0d1a7be
TomasLagger1 <tlagger@udesa.edu.ar>
2025-09-03 00:28:11 -0300

    hbrvirvbieubiruodcliwu

--- pi3/src/components/Header/Header.js
+++ pi3/src/components/Header/Header.js
@@ -4,10 +4,16 @@ function Header() {
   return (
     <header>
         <img src="/img/logo.png" alt="logo"></img>
-        <p>Reels+</p>
-        <nav class="naaaaaavbar">
-             <a href="/">Home</a>
-             <a href="/favoritos">Favoritos</a>
+        <h1>Reels+</h1>
+        <nav className="naaaaaavbar">
+            <div className="linkContainer">
+                <a href="/">Home</a>
+                <a href="/favoritos">Favoritos</a>
+            </div>
+             <form className="search-form" action="" method="get">
+                <input type="text" className="" name="searchData" placeholder="Buscar..." value="" />
+                <button type="submit" className="btn btn-success btn-sm">Buscar</button>
+            </form>
         </nav>
     </header>
   )
