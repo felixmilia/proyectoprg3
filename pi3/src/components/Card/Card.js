eb1627a6da362ac5b87015dcea04d7e54d7107c8
fMmUd <mongiatfranco@udesa.edu.ar>
2025-09-14 17:32:53 -0300

    componente card , lowder en popular y manejo de descripcion

---
+++ pi3/src/components/Card/Card.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            mostrarDescripcion: false , 
+            textoDescripcion: "Ver descripcion"
+        };
+    }
+
+    componentDidMount() {
+       
+    }
+
+    manejarDescripcion() {
+        if (this.state.mostrarDescripcion){
+            this.setState({mostrarDescripcion: false, textoDescripcion:" Ver descripcion"})
+
+        }
+        else {
+            this.setState({mostrarDescripcion: true, textoDescripcion:" Ocultar descripcion"})
+
+
+        }
+        
+    }
+
+
+
+
+
+    render() {
+        const pelicula = this.props.data
+        return (
+        <article className="single-card-playing" key={pelicula.id}>
+            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} className="card-img-top" alt={pelicula.title} />
+            <div className="cardBody">
+                <h5 className="card-title">{pelicula.title}</h5>
+               {this.state.mostrarDescripcion ? <p className="card-text">{pelicula.overview}</p>: ""}
+                <div className="card-actions">
+                    <button onClick= {()=>this.manejarDescripcion()}className="btn btn-primary">{this.state.textoDescripcion}</button>
+                    <a href="#" className="btn btn-primary" onClick={event => {event.preventDefault(); this.props.history.push(`/detalle/${pelicula.id}`);}}>
+                        Ir a detalle
+                    </a>
+                    <button className="btn alert-primary">♥️</button>
+                </div>
+            </div>
+        </article>
+         
+        );
+    }
+}
+
+export default Card;
--- pi3/src/components/Populares/Populares.js
+++ pi3/src/components/Populares/Populares.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './styles.css';
-
+import Card from "../Card/Card"
 class Populares extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            peliculas: []
+            peliculas: [], 
+            cargando: true
         };
     }
 
     componentDidMount() {
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=702201d43b610a61ca14f4b8810d7ccb&language=es-ES&page=1')
             .then(response => response.json())
-            .then(data => this.setState({ peliculas: data.results }))
+            .then(data => this.setState({ peliculas: data.results, cargando: false }))
             .catch(error => console.log('El error fue: ' + error));
     }
 
@@ -21,23 +22,11 @@ class Populares extends Component {
         const peliculasAMostrar = this.state.peliculas.slice(0, 4);
 
         return (
-            <section className="row cards" id="now-playing">
-                {peliculasAMostrar.map(pelicula => (
-                    <article className="single-card-playing" key={pelicula.id}>
-                        <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} className="card-img-top" alt={pelicula.title} />
-                        <div className="cardBody">
-                            <h5 className="card-title">{pelicula.title}</h5>
-                            <p className="card-text">{pelicula.overview}</p>
-                            <div className="card-actions">
-                                <a href="#" className="btn btn-primary">Ver descripción</a>
-                                <a href="#" className="btn btn-primary" onClick={event => {event.preventDefault(); this.props.history.push(`/detalle/${pelicula.id}`);}}>
-                                    Ir a detalle
-                                </a>
-                                <button className="btn alert-primary">♥️</button>
-                            </div>
-                        </div>
-                    </article>
-                ))}
+            <section className="row cards" id="now-playing"> 
+                {this.state.cargando ? <p> cargando </p> : peliculasAMostrar.map(pelicula => (
+                    <Card data={pelicula} /> 
+                )) }
+                
                 <div>
                     <button className="btn btn-primary" onClick={() => this.props.history.push('/ver-mas-popular')}>
                         Ver más películas
